6e8fe0d2a043b073e9549e299048dc2c38a6bf85
Quentin Lepine <quentin.lpn25@gmail.com>
2023-08-31 21:29:34 +0200

    [added] compatibility with Controllable

--- src/main/java/com/getitemfromblock/create_tweaked_controllers/ModClientStuff.java
+++ src/main/java/com/getitemfromblock/create_tweaked_controllers/ModClientStuff.java
@@ -1,16 +1,26 @@
 package com.getitemfromblock.create_tweaked_controllers;
 
+import com.getitemfromblock.create_tweaked_controllers.compat.ControllerHandler;
 import com.getitemfromblock.create_tweaked_controllers.config.ModKeyMappings;
 import com.getitemfromblock.create_tweaked_controllers.controller.TweakedLinkedControllerClientHandler;
 import com.getitemfromblock.create_tweaked_controllers.input.MouseCursorHandler;
 
 import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.client.gui.OverlayRegistry;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ModClientStuff
 {
+    private static boolean controllableLoaded = false;
+
+    public static boolean isControllableLoaded()
+    {
+        return controllableLoaded;
+    }
+    
     public static void onConstructor(IEventBus modEventBus, IEventBus forgeEventBus)
     {
         modEventBus.addListener(ModClientStuff::clientInit);
@@ -18,6 +28,11 @@ public class ModClientStuff
 
     public static void clientInit(final FMLClientSetupEvent event)
     {
+        if(ModList.get().isLoaded("controllable"))
+        {
+            controllableLoaded = true;
+            MinecraftForge.EVENT_BUS.register(new ControllerHandler());
+        }
         MouseCursorHandler.InitValues();
         ModKeyMappings.init();
         OverlayRegistry.registerOverlayAbove(ForgeIngameGui.HOTBAR_ELEMENT, "Create: Tweaked Controller's Tweaked Linked Controller", TweakedLinkedControllerClientHandler.OVERLAY);
---
+++ src/main/java/com/getitemfromblock/create_tweaked_controllers/compat/ControllerHandler.java
@@ -0,0 +1,61 @@
+package com.getitemfromblock.create_tweaked_controllers.compat;
+
+import java.util.Map;
+
+import com.getitemfromblock.create_tweaked_controllers.CreateTweakedControllers;
+import com.getitemfromblock.create_tweaked_controllers.controller.TweakedLinkedControllerClientHandler;
+
+import com.mrcrayfish.controllable.event.ControllerEvent;
+import com.mrcrayfish.controllable.event.GatherActionsEvent;
+import com.mrcrayfish.controllable.client.Action;
+import com.mrcrayfish.controllable.client.ActionVisibility;
+import com.mrcrayfish.controllable.client.ButtonBinding;
+import com.mrcrayfish.controllable.client.ButtonBindings;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+@OnlyIn(Dist.CLIENT)
+public class ControllerHandler
+{
+    @SubscribeEvent
+    public void onButtonInput(ControllerEvent.ButtonInput event)
+    {
+        if(event.getState() && event.getButton() != ButtonBindings.USE_ITEM.getButton() && TweakedLinkedControllerClientHandler.MODE != TweakedLinkedControllerClientHandler.Mode.IDLE)
+        {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onMoveEvent(ControllerEvent.Move event)
+    {
+        if(TweakedLinkedControllerClientHandler.MODE == TweakedLinkedControllerClientHandler.Mode.IDLE)
+            return;
+            
+        event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public void onTurnEvent(ControllerEvent.Turn event)
+    {
+        if(TweakedLinkedControllerClientHandler.MODE == TweakedLinkedControllerClientHandler.Mode.IDLE)
+            return;
+            
+        event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public void onAvailableActions(GatherActionsEvent event)
+    {
+        if(TweakedLinkedControllerClientHandler.MODE == TweakedLinkedControllerClientHandler.Mode.IDLE)
+            return;
+
+        Map<ButtonBinding, Action> actionMap = event.getActions();
+        actionMap.remove(ButtonBindings.ATTACK);
+        actionMap.remove(ButtonBindings.OPEN_INVENTORY);
+        actionMap.put(ButtonBindings.USE_ITEM, new Action(CreateTweakedControllers.translateDirect("keybind.controller_exit"), Action.Side.LEFT));
+    }
+}
